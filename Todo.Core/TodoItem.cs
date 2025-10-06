96f8c98401943d0ef0317a86268d8052bb915e9c
AleksandrMeshkov <mrkethss@gmail.com>
2025-10-06 16:49:28 +0700

    add new function

--- Todo.Core/TodoItem.cs
+++ Todo.Core/TodoItem.cs
@@ -9,22 +9,36 @@ namespace Todo.Core
 {
     public class TodoItem
     {
-        
-            public Guid Id { get; } = Guid.NewGuid();
-            public string Title { get; private set; }
-            public bool IsDone { get; private set; }
-            public TodoItem(string title)
-            {
-                Title = title?.Trim() ?? throw new ArgumentNullException(nameof(title));
-            }
-            public void MarkDone() => IsDone = true;
-            public void MarkUndone() => IsDone = false;
-            public void Rename(string newTitle)
-            {
-                if (string.IsNullOrWhiteSpace(newTitle)) throw new ArgumentException("Title required", nameof(newTitle));
-               
-                Title = newTitle.Trim();
-            }
+        [JsonPropertyName("id")]
+        public Guid Id { get; } = Guid.NewGuid();
+
+        [JsonPropertyName("title")]
+        public string Title { get; private set; }
+        [JsonPropertyName("isDone")]
+        public bool IsDone { get; private set; }
+
+        [JsonConstructor]
+        public TodoItem(Guid id, string title, bool isDone)
+        {
+            Id = id;
+            Title = title;
+            IsDone = isDone;
         }
+
+        public TodoItem(string title)
+        {
+            Title = title?.Trim() ?? throw new ArgumentNullException(nameof(title));
+        }
+
+        public void MarkDone() => IsDone = true;
+        public void MarkUndone() => IsDone = false;
+        public void Rename(string newTitle)
+        {
+            if (string.IsNullOrWhiteSpace(newTitle)) throw new ArgumentException("Titlerequired", nameof(newTitle));
+
+            Title = newTitle.Trim();
+        }
+
+    }
     
 }
